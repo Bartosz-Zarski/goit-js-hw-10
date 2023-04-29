6351a41b56d273412620e9951d24c9753bcfb5e5
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-04-29 21:43:55 +0200

    Search almost finished

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+const searchParams = new URLSearchParams({
+  fields: "name,capital,population,flags,languages"
+});
+const API_URL = "https://restcountries.com/v3.1/name"
+console.log(searchParams)
+export const fetchCountries = (name) => {
+  return fetch(`${API_URL}/${name}?${searchParams}`
+  )
+    .then(response => {
+      return response.json();
+    })
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { debounce } from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
+
+const countrySearch = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `
+            <li>
+              ${country.name.common}</p>
+            </li>
+        `;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+  countryList.querySelectorAll('li').forEach(li => {
+    classList.add("country-detail");
+  });
+}
+
+countrySearch.addEventListener(
+  'input',
+  debounce(async ev => {
+    if (!ev.target.value) {
+      countryList.querySelectorAll('li').forEach(li => {
+        li.remove();
+      });
+      return;
+    }
+    const countries = await fetchCountries(ev.target.value)
+      .then(countries => renderCountryList(countries))
+      .catch(error => console.log(error));
+
+    // console.log(countries);
+  }, DEBOUNCE_DELAY)
+);
