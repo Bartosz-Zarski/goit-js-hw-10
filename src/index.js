8e1036cf6b1fbaa17047a46d5cb3553c1a278480
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-05-04 10:11:04 +0200

    Added styling and country details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import { debounce } from 'lodash';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -8,35 +9,44 @@ const countrySearch = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-function renderCountryList(countries) {
-  const markup = countries
-    .map(country => {
-      return `
-            <li>
-              ${country.name.common}</p>
-            </li>
-        `;
-    })
-    .join('');
-  countryList.innerHTML = markup;
-  countryList.querySelectorAll('li').forEach(li => {
-    li.classList.add("country-detail");
-  });
-}
-
 countrySearch.addEventListener(
   'input',
   debounce(async ev => {
-    if (!ev.target.value) {
-      countryList.querySelectorAll('li').forEach(li => {
-        li.remove();
-      });
+
+    // Usunięcie listy i szczegolow kraju
+    const countryName = ev.target.value;
+    if (countryName === '') {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
       return;
     }
-    const countries = await fetchCountries(ev.target.value)
-      .then(countries => renderCountryList(countries))
-      .catch(error => console.log(error));
 
-    // console.log(countries);
+    const countries = await fetchCountries(countryName.trim());
+
+    // Stworzenie listy krajow
+    if (countries.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (countries.length <= 10) {
+      countryList.innerHTML = countries
+        .map(
+          country =>
+            `<li><svg height="16" width="24"class="country__Flag" src="${country.flags.png}" />${country.name.common}</li>`
+        )
+        .join('');
+    }
+
+    // Utworzenie szczegolow kraju
+    if (countries.length === 1) {
+      countryInfo.innerHTML = `
+      <p><strong>Capital:</strong> ${countries[0].capital}</p>
+      <p><strong>Population:</strong> ${countries[0].population}</p>
+      <p><strong>Languages:</strong> ${Object.values(
+        countries[0].languages
+      ).join(',')}</p>`;
+    }
+
+    if (!countries.length) {
+      Notify.failure('No matches found.');
+    }
   }, DEBOUNCE_DELAY)
 );
